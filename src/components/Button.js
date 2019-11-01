20f5a2b81eaa80593eb37c3b5db0a61947f36289
JOHN NELSON <JohnNelson4850@gmail.com>
2019-11-01 13:52:53 -0700

    app/header/footer

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 const Button = ({ button, radio, text }) => {
   return (
-    <label> 
-     {button && <button type="submit">{text}</button>}
-     {radio && <input type="radio">{text}</input>}
+    <label>
+      {button && <button type="submit">{text}</button>}
+      {radio && <input type="radio">{text}</input>}
     </label>
-  )
-}
+  );
+};
 
-Button.propType = {
+Button.propTypes = {
   text: PropTypes.string.isRequired,
   radio: PropTypes.string,
   button: PropTypes.string
-}
+};
 
 export default Button;

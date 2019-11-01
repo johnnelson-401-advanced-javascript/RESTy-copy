37993b9544c4bcdbee63d4696c3cd760ae33bcfe
adoxic <griffinmaeve@gmail.com>
2019-11-01 15:21:05 -0700

    built out form

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default Input = ({ method, myFunc }) => {
+const Input = ({ updateMethod, submit }) => {
 
+  return (
+    <form>
+      <input type='text' name='url'></input>
+      <label>
+        <input name='method' type="radio" value='get' onClick={() => updateMethod}>Get</input>
+        <input name='method' type="radio" value='post' onClick={() => updateMethod}>Post</input>
+        <input name='method' type="radio" value='put' onClick={() => updateMethod}>Put</input>
+        <input name='method' type="radio" value='patch' onClick={() => updateMethod}>Patch</input>
+        <input name='method' type="radio" value='delete' onClick={() => updateMethod}>Delete</input>
+      </label>
+      <button type="submit" onSubmit={submit}>Go</button>
+      <input type='text' name='json'></input>
+    </form>
+     
+  );
+};
 
-    const buttons = [{}]
-    return (
-       
-       <>
-  <form>
-        <input type='text' name='url-input'></input>
-        <label>
-          {radio && <input name='method' type="radio" value={text}>{text}</input>}
-          {button && <button type="submit">Go</button>}
-        </label>
+Input.propTypes = {
+  submit: PropTypes.func.isRequired,
+  updateMethod: PropTypes.func.isRequired
+};
 
-  </form>
-        </>
-        )
-        }
-      }
-    };
-    
-ButtonBar.propTypes = {
-          ButtonBar: PropTypes.array.isRequired
-      };
+export default Input;
+

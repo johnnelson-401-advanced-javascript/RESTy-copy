57075f4b77224c34c97fc023054afe01037be683
adoxic <griffinmaeve@gmail.com>
2019-11-01 13:30:33 -0700

    button component

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ button, radio, text }) => {
+  return (
+    <label> 
+     {button && <button type="submit">{text}</button>}
+     {radio && <input type="radio">{text}</input>}
+    </label>
+  )
+}
+
+Button.propType = {
+  text: PropTypes.string.isRequired,
+  radio: PropTypes.string,
+  button: PropTypes.string
+}
+
+export default Button;

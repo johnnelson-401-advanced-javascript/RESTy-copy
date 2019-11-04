e18b85eba5ab46d176771cc923a70578139400b1
adoxic <griffinmaeve@gmail.com>
2019-11-04 09:40:27 -0800

    more css

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './Inputs.css';
 
 const Input = ({ updateMethod, submit, updateURL, updateJSON }) => {
 
   return (
-    <form onSubmit={submit}>
+    <form onSubmit={submit} className={style.Inputs}>
       <input type='text' name='url' onChange={({ target }) => updateURL(target.value)}/>
       <label>
-        <input name='method' type="radio" value='get' onClick={() => updateMethod('get')}/>
-        <input name='method' type="radio" value='post' onClick={() => updateMethod('post')}/>
-        <input name='method' type="radio" value='put' onClick={() => updateMethod('put')}/>
-        <input name='method' type="radio" value='patch' onClick={() => updateMethod('patch')}/>
-        <input name='method' type="radio" value='delete' onClick={() => updateMethod('delete')}/>
+        <input name='method' type="radio" value='get' id='get' onClick={() => updateMethod('get')}/>
+        <label htmlFor='get'>Get</label>
+        <input name='method' type="radio" value='post' id='post' onClick={() => updateMethod('post')}/>
+        <label htmlFor='post'>Post</label>
+        <input name='method' type="radio" value='put' id='put' onClick={() => updateMethod('put')}/>
+        <label htmlFor='put'>Put</label>
+        <input name='method' type="radio" value='patch' id='patch' onClick={() => updateMethod('patch')}/>
+        <label htmlFor='patch'>Patch</label>
+        <input name='method' type="radio" value='delete' id='delete' onClick={() => updateMethod('delete')}/>
+        <label htmlFor='delete'>Delete</label>
       </label>
       <button type="submit">Go</button>
       <input type='text' name='json' onChange={({ target }) => updateJSON(target.value)}/>
